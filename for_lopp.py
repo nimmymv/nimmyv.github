aa59f35046ac3b739bcc0924779916d0dd6734a9
nimmymv <83362757+nimmymv@users.noreply.github.com>
2024-05-13 14:51:22 +0530

    Update for_lopp.py

--- for_lopp.py
+++ for_lopp.py
@@ -4,5 +4,5 @@ for i in range(i+1):
 j= str(input("enter alphabet:"))
 for j in range(ord('a'),ord('z')+1):
     print(chr(j))
-for j in range(ord('A'),ord('Z')+1):
-    print(chr(j))
+for k in range(ord('A'),ord('Z')+1):
+    print(chr(k))
