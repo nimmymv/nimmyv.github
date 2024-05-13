66f094e5bc359d976f582b1bbd99b05a5e393b69
nimmymv <nimmyv2017@gmail.com>
2024-05-13 17:31:43 +0530

    function_n_list starting

---
+++ function_starting.py
@@ -0,0 +1,3 @@
+def full_name(name):
+    return "Nimmy" + " " + name
+print(full_name("varghese"))
---
+++ list.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3,5,6]
+print(numbers[0])
+numbers.append(7)
+print("*******")
+
+for i in numbers:
+    print(i)
+numbers.insert(3,4)
+
+print("*******")
+for i in numbers:
+     print(i)
